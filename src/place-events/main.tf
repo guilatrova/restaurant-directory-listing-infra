29b32e6a640287dfda6c2f07234181e5bbf07998
Guilherme Latrova <hello@guilatrova.dev>
2021-11-21 08:58:12 -0300

    feat: add role for aws role

--- src/place-events/main.tf
+++ src/place-events/main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn = ""
+    role_arn = var.aws_role
   }
 }
---
+++ src/place-events/vars.tf
@@ -0,0 +1,3 @@
+variable "aws_role" {
+    default = "arn:aws:iam::230622729620:role/AWSRestaurantDirectoryListingRole"
+}
